bf79eb1cfc78e16cc59a9754b54468bb82bfdc4d
mamonggjhb024 <mamonggjhb024@student.wethinkcode.co.za>
2025-07-27 16:15:29 +0200

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to My Calculator");
+        System.out.println("Enter first number");
+        int numb1 = scanner.nextInt();
+
+        System.out.println("Enter second number");
+        int numb2 = scanner.nextInt();
+
+
+    }
+}
